c157379d4458947d938cf0cbdaa4cab57a5f74bf
soulierc <clement.soulier@iut-valence.fr>
2014-02-11 08:25:57 +0100

    Fixed bug with git repositories

--- Workshop/src/Workshop/UserPolling.java
+++ Workshop/src/Workshop/UserPolling.java
@@ -4,4 +4,6 @@ public interface UserPolling {
 	public void writeSavedInstructionsInSavefile();
 	public void recordInstructions();
 	public void sendFile();
+	void saveOneInstruction(byte codeOp, int nbArg, byte[] args);
+	void requestDisplayOfPrimitiveInstructions();
 }
